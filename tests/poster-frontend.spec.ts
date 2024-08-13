fdacbe0d0092f4935aefc30a19a2de9eef907621
Davyd Kondratenko <dajan526@gmail.com>
2024-08-14 01:04:02 +0200

    feat: add test for post deletion

--- tests/poster-frontend.spec.ts
+++ tests/poster-frontend.spec.ts
@@ -1,11 +1,14 @@
-import { describe } from "node:test";
-
-const { test, expect } = require("@playwright/test");
+const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Poster Frontend", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("http://localhost:5173");
+  beforeEach(async ({ page, request }) => {
+    // Resetting(clearing) posts collection at database
+    await request.post("http://localhost:3001/test/reset");
 
+    await page.goto("http://localhost:5173");
+  });
+
+  test("front page can be opened", async ({ page }) => {
     await expect(page.getByText("Poster")).toBeVisible();
     await expect(
       page.getByText("Made using React, Redux, Express and MongoDB")
@@ -13,8 +16,36 @@ describe("Poster Frontend", () => {
   });
 
   test("post can be added", async ({ page }) => {
-    await page.goto("http://localhost:5173");
-
     await page.getByRole("button").click();
+    await page
+      .getByTestId("addPostTextarea")
+      .fill("A post created by playwright");
+    await page.getByRole("button", { name: "ADD POST" }).click();
+
+    await expect(
+      page.getByTestId("postExcerptContent", {
+        name: "A post created by playwright",
+      })
+    ).toBeVisible();
+  });
+
+  describe("post management", () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole("button").click();
+      await page
+        .getByTestId("addPostTextarea")
+        .fill("A post created by playwright");
+      await page.getByRole("button", { name: "ADD POST" }).click();
+    });
+
+    test("post can be deleted", async ({ page }) => {
+      await page
+        .getByTestId("postExcerpt", { name: "A post created by playwright" })
+        .click();
+      await page.getByTestId("deleteBtn").click();
+      await page.getByTestId("deleteModalDeleteConfirmationBtn").click();
+
+      await expect(page.getByText("No posts found")).toBeVisible();
+    });
   });
 });
