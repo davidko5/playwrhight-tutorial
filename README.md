21b330c5ddc7365a8cc78e10619032d9e7db4660
Davyd Kondratenko <dajan526@gmail.com>
2024-08-24 20:31:38 +0200

    feat: add tests covering entities crud Added tests that cover crud of posts, comments, replies and their rating.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Testing using playwright
 
 Used [this tutorial](https://fullstackopen.com/en/part5/end_to_end_testing_playwright)
+
+For testing in debug mode use this script:  ***npm test -- -g 'post can be rated' --debug --project chromium***<br>
+Change ***'post can be rated'*** to your specific test name.
--- tests/poster-frontend.spec.ts
+++ tests/poster-frontend.spec.ts
@@ -1,51 +1,138 @@
-const { test, expect, beforeEach, describe } = require("@playwright/test");
+import { test, expect, Locator } from "@playwright/test";
+import {
+  addComment,
+  addPost,
+  addReply,
+  entryCanBeDeleted,
+  entryCanBeEdited,
+  entryCanBeRated,
+} from "./helper";
+
+const { describe, beforeEach } = test;
+
+const postContent = "A post created by playwright";
+const secondPostContent = "A second post created by playwright";
+const postContentEdited = "Edited post contend";
+const commentContent = "A comment added by playwright";
+const commentContentEdited = "Edited comment content";
+const replyContent = "A reply added by playwright";
+const replyContentEdited = "Edited reply content";
 
 describe("Poster Frontend", () => {
   beforeEach(async ({ page, request }) => {
     // Resetting(clearing) posts collection at database
     await request.post("http://localhost:3001/test/reset");
-
     await page.goto("http://localhost:5173");
   });
 
-  test("front page can be opened", async ({ page }) => {
-    await expect(page.getByText("Poster")).toBeVisible();
+  test("Front page can be opened", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", { level: 1, name: "Poster" })
+    ).toBeVisible();
     await expect(
       page.getByText("Made using React, Redux, Express and MongoDB")
     ).toBeVisible();
   });
 
-  test("post can be added", async ({ page }) => {
-    await page.getByRole("button").click();
-    await page
-      .getByTestId("addPostTextarea")
-      .fill("A post created by playwright");
-    await page.getByRole("button", { name: "ADD POST" }).click();
-
-    await expect(
-      page.getByTestId("postExcerptContent", {
-        name: "A post created by playwright",
-      })
-    ).toBeVisible();
+  test("'No posts' modal is visible and has hover style applied", async ({
+    page,
+  }) => {
+    const modal = page.getByTestId("noPostsModal");
+    await expect(modal).toHaveCSS(
+      "box-shadow",
+      "rgb(103, 114, 126) 0px 0px 15px 0px"
+    );
+    await modal.hover();
+    await expect(modal).toHaveCSS(
+      "box-shadow",
+      "rgb(103, 114, 126) 0px 0px 35px 0px"
+    );
   });
 
-  describe("post management", () => {
+  test("Post can be added", async ({ page }) => {
+    await addPost(page, postContent);
+    await expect(page.getByTestId("postExcerptContent")).toBeVisible();
+  });
+
+  test("Two posts can be added and navigated", async ({ page }) => {
+    await addPost(page, postContent);
+    await addPost(page, secondPostContent);
+
+    await page.getByText(secondPostContent).locator("..").click();
+
+    await expect(page.getByTestId("content")).toHaveText(secondPostContent);
+  });
+
+  describe("Post management", () => {
+    let post: Locator;
     beforeEach(async ({ page }) => {
-      await page.getByRole("button").click();
-      await page
-        .getByTestId("addPostTextarea")
-        .fill("A post created by playwright");
-      await page.getByRole("button", { name: "ADD POST" }).click();
+      await addPost(page, postContent);
+      await page.getByTestId("postExcerpt").click();
+      post = page.getByTestId("postContainer");
     });
 
-    test("post can be deleted", async ({ page }) => {
-      await page
-        .getByTestId("postExcerpt", { name: "A post created by playwright" })
-        .click();
-      await page.getByTestId("deleteBtn").click();
-      await page.getByTestId("deleteModalDeleteConfirmationBtn").click();
+    test("Post can be rated", async () => {
+      await entryCanBeRated(post);
+    });
 
-      await expect(page.getByText("No posts found")).toBeVisible();
+    test("Post can be deleted", async ({ page }) => {
+      await entryCanBeDeleted(page, post);
+    });
+
+    test("Post can be edited", async ({ page }) => {
+      await entryCanBeEdited(post, postContentEdited);
+    });
+
+    test("Comment can be added", async ({ page }) => {
+      await addComment(page, commentContent);
+      expect(page.getByTestId("postComment")).toContainText(commentContent);
+    });
+
+    describe("comment management", () => {
+      let comment: Locator;
+      beforeEach(async ({ page }) => {
+        await addComment(page, commentContent);
+        comment = page.getByTestId("postComment");
+      });
+
+      test("Comment can be rated", async ({ page }) => {
+        await entryCanBeRated(comment);
+      });
+
+      test("Comment can be deleted", async ({ page }) => {
+        await entryCanBeDeleted(page, comment);
+      });
+
+      test("Comment can be edited", async () => {
+        await entryCanBeEdited(comment, commentContentEdited);
+      });
+
+      test("Reply to a comment can be added", async ({ page }) => {
+        addReply(page, replyContent);
+        await expect(page.getByTestId("commentReply")).toContainText(
+          replyContent
+        );
+      });
+
+      describe("Reply management", () => {
+        let reply: Locator;
+        beforeEach(async ({ page }) => {
+          await addReply(page, replyContent);
+          reply = page.getByTestId("commentReply");
+        });
+
+        test("Reply can be rated", async () => {
+          await entryCanBeRated(reply);
+        });
+
+        test("Reply can be deleted", async ({ page }) => {
+          await entryCanBeDeleted(page, reply);
+        });
+
+        test("Reply can be edited", async () => {
+          await entryCanBeEdited(reply, replyContentEdited);
+        });
+      });
     });
   });
 });
